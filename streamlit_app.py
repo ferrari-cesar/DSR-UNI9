12505e2cea8f820fe4dac68619ab7efc461bc4df
ferrari-cesar <ferrari_cesar@hotmail.com>
2024-06-09 21:34:26 -0300

    new5

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,21 +62,27 @@ experiencia = st.selectbox('Experiência:', experiencia_options)
 # Define the questions for the Likert scale
 questions = [
     "Eu tenho uma compreensão clara de onde estamos indo",
-    "Eu tenho uma noção clara de onde quer que nossa unidade se encaixe",
-    "Eu tenho as ferramentas e recursos necessários para realizar o meu trabalho",
-    "Eu recebo feedback regular sobre o meu desempenho",
-    "Eu me sinto valorizado pelo meu trabalho",
-    "Eu confio na liderança da minha unidade",
-    "Eu entendo como o meu trabalho contribui para os objetivos da empresa",
-    "Eu sinto que há oportunidades de crescimento na empresa",
-    "Eu me sinto parte de uma equipe",
-    "Eu sinto que minha opinião é valorizada"
+    "Eu tenho uma noção clara de onde quer que nossa unidade estará em 5 anos",
+    "Eu não tenho ideia de onde a organização está indo",
+    "Eu digo coisas que deixam os funcionários orgulhosos de pertencer a essa organização",
+    "Eu digo coisas positivas sobre o grupo de trabalho",
+    "Eu incentivo as pessoas a ver os ambientes em mudança como situações cheias de oportunidades",
+    "Eu desafio os membros da minha equipe a pensar sobre velhos problemas de novas maneiras",
+    "Eu tenho ideias que forçam os membros da equipe a repensarem algumas coisas que nunca questionei antes",
+    "Eu desafio os membros da minha equipe a repensarem algumas das minhas suposições básicas sobre o meu trabalho",
+    "Eu considero os sentimentos pessoais dos membros da minha equipe antes de agir",
+    "Eu me comporto de uma maneira que considera as necessidades pessoais dos membros da minha equipe",
+    "Eu estou atento a que os interesses dos funcionários recebem a devida consideração",
+    "Eu elogio os membros da equipe quando fazem um trabalho acima da média",
+    "Eu reconheço a melhora na qualidade do trabalho dos membros da minha equipe",
+    "Eu elogio pessoalmente os membros da minha equipe quando fazem um trabalho"
 ]
+
 
 # Collect responses
 responses = {}
 for question in questions:
-    response = st.slider(question, 1, 5, 3)
+    response = st.slider(question, 1, 7, 4)
     responses[question] = response
 
 # Convert responses to DataFrame for visualization
